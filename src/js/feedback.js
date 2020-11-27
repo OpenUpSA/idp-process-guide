a0b860682bf9083382e3000b99b626c2a3f316ca
Paul Watson <github.com@paulmwatson.com>
2020-11-27 13:32:22 +0200

    Only load HotJar feedback if HOT_JAR_SITE_ID is set.

--- src/js/feedback.js
+++ src/js/feedback.js
@@ -1,16 +1,21 @@
+require('dotenv').config();
+
 export default class Feedback {
     constructor() {
         this.addHotJar()
     }
 
     addHotJar = () => {
-        (function(h,o,t,j,a,r){
-            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:2120504,hjsv:6};
-            a=o.getElementsByTagName('head')[0];
-            r=o.createElement('script');r.async=1;
-            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-            a.appendChild(r);
-        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        const HOT_JAR_SITE_ID = `${process.env.HOT_JAR_SITE_ID}`;
+        if (HOT_JAR_SITE_ID !== "undefined") {
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:HOT_JAR_SITE_ID,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        }
     }
 }
