fc5036f936a4a1aeef3ee3e0a1483cda238f5abc
Paul Watson <github.com@paulmwatson.com>
2020-11-27 13:11:10 +0200

    Removed commented out code

--- src/js/load.js
+++ src/js/load.js
@@ -1,10 +1,8 @@
-import {Marker} from "./marker";
 import {Engagements} from "./engagements";
 import {Geography} from "./geography";
 
 export class Load {
     constructor(baseUrl, hostname, analytics) {
-        //const marker = new Marker();
         const engagements = new Engagements(baseUrl, hostname, analytics);
         const geography = new Geography(baseUrl, hostname);
     }
