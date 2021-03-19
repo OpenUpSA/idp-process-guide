6865bb8485f6518f4ff9d89344b2c805f8463bb9
Paul Watson <github.com@paulmwatson.com>
2021-03-19 12:05:42 +0200

    Fix typo; tag => tagHead

--- src/js/webflow/import.js
+++ src/js/webflow/import.js
@@ -1,6 +1,6 @@
 exports.transformDOM = function (window, $) {
   const tagHead = window.document.createElement("script");
-  tag.setAttribute("src", "js/head-load.js");
+  tagHead.setAttribute("src", "js/head-load.js");
   window.document.head.appendChild(tagHead);
 
   const tag = window.document.createElement("script");
