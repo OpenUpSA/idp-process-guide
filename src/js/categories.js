0cf03eef4c6e662a9bbe957c980103e6a10d3306
Paul Watson <github.com@paulmwatson.com>
2021-04-30 14:59:37 +0200

    Add generic, grouped IDP info section based on Categories from database

--- src/js/categories.js
+++ src/js/categories.js
@@ -16,32 +16,35 @@ export class Categories {
   };
 
   showCategories = (categories) => {
+    let currentGroupString = '';
+    let currentGroup = null;
     categories.forEach((category) => {
+      if (category.group !== currentGroupString) {
+        currentGroupString = category.group;
+        let newGroup = $(".idp-info__template").first().clone(true);
+        $(newGroup).find('.subheading')[0].textContent = category.group;
+        $(newGroup).removeClass('hidden')
+        newGroup.appendTo("#engagement-activity-types .container");
+        currentGroup = newGroup;
+      }
+
       let newCategory = $(".engagement-type").first().clone(true);
       $(newCategory).removeClass('hidden')
       $(newCategory).find(".engagement-heading")[0].textContent = category.name;
       $(newCategory).find(".engagement-icon span")[0].className = category.icon;
-      $(newCategory).find(".engagement__rich-text p")[0].textContent =
-        category.description;
+      $(newCategory).find(".engagement__rich-text").first().html(category.description);
       $(newCategory).find(".engagement-icon span")[0].textContent = "";
-      $(newCategory).find(".engagement-type__content")[0].style.height =
-        "unset";
+      $(newCategory).find(".engagement-type__content")[0].style.height = "unset";
       $(newCategory)
         .find(".engagement-type__content")
         .animate({ height: "toggle" });
-
       $(newCategory)
         .find(".engagement-type__header")
         .first()
         .click(this.toggleCategory);
-        if (category.public) {
-          newCategory.appendTo(".public-engagement-types__wrapper");
-        } else {
-          newCategory.appendTo(".activity-types__wrapper");
-        }
+      newCategory.appendTo(currentGroup.find('.idp-info__wrapper'));
+      $(currentGroup).find('.loading').first().remove();
     });
-    $('.public-engagement-types__wrapper .loading').remove();
-    $('.activity-types__wrapper .loading').remove();
     $('.engagement-type').first().remove()
   };
 
